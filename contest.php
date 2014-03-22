a4e6ef82c5312cc9be3587ee061c14114bb3ce88
Joel Vandal <joel@vandal.ca>
2014-03-22 09:04:08 -0400

    - Update version.

--- contest.php
+++ contest.php
@@ -10,7 +10,7 @@
  * Plugin Name:		Photos Contest
  * Plugin URI:        	https://github.com/joelvandal/wordpress-photos-contest/wiki
  * Description:       	Create Photos Contest
- * Version:           	1.0.3
+ * Version:           	1.0.5
  * Author:       	Joel Vandal
  * Author URI:       	http://joel.vandal.ca/
  * Text Domain:       	photoscontest
